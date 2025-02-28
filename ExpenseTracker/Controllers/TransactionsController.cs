35d014d2d13a0e807b452220da1db5b3e48173aa
Nuri Işık <nurii6345@gmail.com>
2025-02-28 16:10:18 +0300

    #Transactions Index Controller ve View oluşturuldu

---
+++ ExpenseTracker/Controllers/TransactionsController.cs
@@ -0,0 +1,29 @@
+﻿using ExpenseTracker.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace ExpenseTracker.Controllers
+{
+    public class TransactionsController : Controller
+    {
+
+        private readonly ApplicationDbContext _context;
+
+        public TransactionsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var transactions = await _context.Transactions.ToListAsync();
+            return View(transactions);
+        }
+
+
+        //public IActionResult Index()
+        //{
+        //    return View();
+        //}
+    }
+}
